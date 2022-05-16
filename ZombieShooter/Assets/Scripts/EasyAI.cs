8186b113802fa80406c24967a136955016d76b43
Łukasz Jagieła <52582469+DevLukaHere@users.noreply.github.com>
2022-05-16 14:09:09 +0200

    Initial Commit
    .gitignore
    .gitattributes

---
+++ ZombieShooter/Assets/Scripts/EasyAI.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EasyAI : MonoBehaviour
+{
+    public CharacterController characterControler;
+    private Transform player;
+    private Transform enemy;
+
+    public float rotationSpeed = 4.0f;
+    public float moveSpeed = 5.0f;
+    public float range = 30.0f;
+
+    public float jumpHeight = 0f;
+    public float whenStop = 0f;
+
+    void Start()
+    {
+        characterControler = GetComponent<CharacterController>();
+        enemy = transform;
+        GameObject go = GameObject.FindWithTag("Player");
+        player = go.transform;
+    }
+
+    void Update()
+    {
+        float range = Vector3.Distance(enemy.position, player.position);
+        if (range < this.range && range > whenStop)
+        {
+            Vector3 graczPoz = new Vector3(player.position.x, player.position.y, player.position.z);
+            enemy.rotation = Quaternion.Slerp(enemy.rotation, Quaternion.LookRotation(graczPoz - enemy.position), moveSpeed * Time.deltaTime);
+
+            if (!characterControler.isGrounded)
+            {
+                jumpHeight += Physics.gravity.y * Time.deltaTime;
+            }
+
+            Vector3 move = new Vector3(enemy.forward.x, jumpHeight, enemy.forward.z);
+            characterControler.Move(move * moveSpeed * Time.deltaTime);
+        }
+
+
+    }
+}
---
+++ ZombieShooter/Assets/Scripts/JumpScare.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpScare : MonoBehaviour
+{
+    public AudioSource Scream;
+    public GameObject Zombie;
+    void OnTriggerEnter()
+    {
+        Scream.Play();
+        Zombie.SetActive(true);
+        StartCoroutine(EndJump());
+    }
+
+    IEnumerator EndJump()
+    {
+        yield return new WaitForSeconds(2.0f);
+        Zombie.SetActive(false);
+    }
+}
